8e9d134caf46cbef36b10b0365c7bb187e5c8f10
TanPowasd <wang1422857@outlook.com>
2025-04-05 23:36:34 +0800

    将MyMath.h中基本的多项式运算(不带取余,乘方,小数)的计算写完完了-基于bigint的高进度

--- Much_In_One/Project1/main.cpp
+++ Much_In_One/Project1/main.cpp
@@ -3,5 +3,6 @@ using namespace std;
 int main() {
 	//cout << QuickModPower(BIH(114), BIH(2222), BIH(12312313213131)).to_str();
 	//cout << QuickPower(BIH(114), BIH(514)).to_str();
-
+	BIH x1 = BIH(1111111)*BIH(213212131321) / BIH(213123131423);
+	cout << x1.to_str();
 }
